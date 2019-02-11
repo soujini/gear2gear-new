02e89e41770b124c20d428cf44f292785a83a0bb
souj <soujanya.venkatesh@gmail.com>
2019-02-11 19:17:16 +0530

    CORS

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -4,7 +4,8 @@
   DATABASE_URL: 'postgres://bltypmsejfdisv:e88eb86f18914916049a49313bb5c7a8044cd50c229e0b55671ace8e4565f4ea@ec2-174-129-236-147.compute-1.amazonaws.com:5432/d2m86fr3a5nu60',
   API_URL: 'https://gear2gear.herokuapp.com',
   SSL:true,
-  ORIGIN:'https://gear2gear.herokuapp.com',
+  // ORIGIN:'https://gear2gear.herokuapp.com',
+  ORIGIN:'https://gear2gear.in',
   //  //heroku config --app gear2gear gives connectionString
   CONNECTION_STRING:'postgres://qoqgsadljgzsgr:dbc104b647622f5e7460c72e7dc31a0b23b94ece8614c864b104b15dc44f2b9d@ec2-50-17-194-129.compute-1.amazonaws.com:5432/dalp1drkjndrse',
 };
