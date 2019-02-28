8fea68229c14891b8a6710ea5021cda2000ddb8f
souj <soujanya.venkatesh@gmail.com>
2019-02-28 16:46:03 +0530

    Home Page Data-Src Image

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -7,6 +7,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit {
+ slide1:string = "https://s3.amazonaws.com/gear2gear/bg3.jpeg";
+ slide2:string = "https://s3.amazonaws.com/gear2gear/xuv500.jpeg";
+ slide3:string = "https://s3.amazonaws.com/gear2gear/bg5.jpg";
 
   constructor() { }
 
