0e7f23efaa91ee62659c28bf54fc8b5c1abf84c2
souj <soujanya.venkatesh@gmail.com>
2019-03-01 12:50:20 +0530

    Accessibility

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../../auth/auth.service';
+import * as $ from 'jquery';
 
 @Component({
   selector: 'app-header',
@@ -12,6 +13,18 @@ export class HeaderComponent implements OnInit {
   constructor(public auth : AuthService) { }
 
   ngOnInit() {
+    $(document).ready(function(){
+      // $(".navbar-toggler-icon").addClass('sr-only');
+  $(".navbar-toggler-icon").attr("aria-label","Toggle Menu");
+
+
+      // click(function(){
+      //   var div = $("div");
+      //   div.animate({left: '100px'}, "slow");
+      //   div.animate({fontSize: '5em'}, "slow");
+      // });
+    });
+
     this.selectedMenu = window.location.pathname.split("/").pop();
   }
 
