4a841a081ac4024548b85f22836b8607661fc7f2
souj <soujanya.venkatesh@gmail.com>
2019-03-01 10:45:59 +0530

    purify

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,9 @@
 var gulp = require('gulp');
 var gzip = require('gulp-gzip');
-var strip = require('gulp-strip-comments');
+var strip = require('gulp-strip-comments');//uninstall this
+const purify = require('gulp-purifycss');
 
-gulp.task('default',  gulp.series(clean, compress));
+gulp.task('default',  gulp.series(clean, compress,x));
 
 function clean(done) {
   // del(['./dist/**/*.*']);
@@ -12,6 +13,24 @@ function compress(done) {
   return gulp.src(['./dist/*.js'])
   .pipe(gzip())
   .pipe(gulp.dest('./dist', {overwrite: true}));
+  done();
+}
+
+function x(done){
+  return gulp.src('./dist/styles.*.css')
+  .pipe(
+    purify(
+      //['./src/app/**/*.ts', './src/app/**/*.html'],
+      ['./dist/*.js', './dist/*.html'],
+      {
+        info: true, // Outputs reduction information (like in the screenshot above)
+        minify: true, // Minifies the files after reduction
+        rejected: false, // Logs the CSS rules that were removed
+        whitelist: ['*transition*', '*dimmer*'] // Ignored css classes
+      }
+    ),
+  )
+  .pipe(gulp.dest('./dist/'));
   done();
 }
 //
