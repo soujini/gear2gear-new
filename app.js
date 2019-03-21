1e89e752eaa2afad32ecfca9cd1c27540a62b3bc
souj <soujanya.venkatesh@gmail.com>
2019-03-21 18:41:58 +0530

    spdy

--- app.js
+++ app.js
@@ -95,22 +95,22 @@ var options = {
   key: privateKey,
   cert: certificate
   spdy: {
-    protocols: [ 'h2', 'spdy/3.1', 'http/1.1' ],
-    plain: false,
-
-    // **optional**
-    // Parse first incoming X_FORWARDED_FOR frame and put it to the
-    // headers of every request.
-    // NOTE: Use with care! This should not be used without some proxy that
-    // will *always* send X_FORWARDED_FOR
-    'x-forwarded-for': true,
-
-    connection: {
-      windowSize: 1024 * 1024, // Server's window size
-
-      // **optional** if true - server will send 3.1 frames on 3.0 *plain* spdy
-      autoSpdy31: false
-    }
+    protocols: [ 'h2', 'spdy/3.1', 'http/1.1' ]
+     plain: false,
+    //
+    // // **optional**
+    // // Parse first incoming X_FORWARDED_FOR frame and put it to the
+    // // headers of every request.
+    // // NOTE: Use with care! This should not be used without some proxy that
+    // // will *always* send X_FORWARDED_FOR
+    // 'x-forwarded-for': true,
+    //
+    // connection: {
+    //   windowSize: 1024 * 1024, // Server's window size
+    //
+    //   // **optional** if true - server will send 3.1 frames on 3.0 *plain* spdy
+    //   autoSpdy31: false
+    // }
   }
 };
 
