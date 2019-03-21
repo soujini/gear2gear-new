7279185238de2eab2dce1cc0d67c9d70e2ff49b9
souj <soujanya.venkatesh@gmail.com>
2019-03-21 18:31:34 +0530

    spdy

--- app.js
+++ app.js
@@ -94,24 +94,24 @@ var certificate = fs.readFileSync('ssl/server.crt').toString();
 var options = {
   key: privateKey,
   cert: certificate
-  // spdy: {
-  //   protocols: [ 'h2', 'spdy/3.1', 'http/1.1' ],
-  //   plain: false,
-  //
-  //   // **optional**
-  //   // Parse first incoming X_FORWARDED_FOR frame and put it to the
-  //   // headers of every request.
-  //   // NOTE: Use with care! This should not be used without some proxy that
-  //   // will *always* send X_FORWARDED_FOR
-  //   'x-forwarded-for': true,
-  //
-  //   connection: {
-  //     windowSize: 1024 * 1024, // Server's window size
-  //
-  //     // **optional** if true - server will send 3.1 frames on 3.0 *plain* spdy
-  //     autoSpdy31: false
-  //   }
-  // }
+  spdy: {
+    protocols: [ 'h2', 'spdy/3.1', 'http/1.1' ],
+    plain: false,
+
+    // **optional**
+    // Parse first incoming X_FORWARDED_FOR frame and put it to the
+    // headers of every request.
+    // NOTE: Use with care! This should not be used without some proxy that
+    // will *always* send X_FORWARDED_FOR
+    'x-forwarded-for': true,
+
+    connection: {
+      windowSize: 1024 * 1024, // Server's window size
+
+      // **optional** if true - server will send 3.1 frames on 3.0 *plain* spdy
+      autoSpdy31: false
+    }
+  }
 };
 
 spdy
