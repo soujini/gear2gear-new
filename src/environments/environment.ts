c2fbb0ec47337a793fcbb879b04f79e0ba48049f
souj <soujanya.venkatesh@gmail.com>
2021-03-19 15:59:25 +0530

    Connection string change ssl

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
       production: true,
       // CONNECTION_STRING:'postgres://localhost:5432/postgres',
       CONNECTION_STRING:'postgres://postgres:1llus10n@postgres:5432',
-      DATABASE_URL:'postgres://localhost:5432/postgres',
+      // DATABASE_URL:'postgres://localhost:5432/postgres',
       // DATABASE_URL: 'postgres://bltypmsejfdisv:e88eb86f18914916049a49313bb5c7a8044cd50c229e0b55671ace8e4565f4ea@ec2-174-129-236-147.compute-1.amazonaws.com:5432/d2m86fr3a5nu60',
       API_URL: 'https://gear2gear.herokuapp.com',
       SSL:false,
@@ -17,9 +17,9 @@
         storageBucket: "gear2gear-3a321.appspot.com",
         messagingSenderId: "894654011390",
         appId: "1:894654011390:web:70329a7e57903272c0d2af",
-        measurementId: "G-Q83EWBD287"    
+        measurementId: "G-Q83EWBD287"
       }
-    
+
 
       //  //heroku config --app gear2gear gives connectionString
       // CONNECTION_STRING:'postgres://qoqgsadljgzsgr:dbc104b647622f5e7460c72e7dc31a0b23b94ece8614c864b104b15dc44f2b9d@ec2-50-17-194-129.compute-1.amazonaws.com:5432/dalp1drkjndrse',
