f7a8a0146f093f2b025006cbc5d4e230eb192560
souj <soujanya.venkatesh@gmail.com>
2021-03-19 15:37:33 +0530

    Connection string change ssl

--- src/api/routes/connection.js
+++ src/api/routes/connection.js
@@ -11,7 +11,7 @@ function connect(){
     connectionString: environment.CONNECTION_STRING,
     // connectionString: "postgres://qoqgsadljgzsgr:dbc104b647622f5e7460c72e7dc31a0b23b94ece8614c864b104b15dc44f2b9d@postgres:5432",
     // connectionString:'postgres://qoqgsadljgzsgr:dbc104b647622f5e7460c72e7dc31a0b23b94ece8614c864b104b15dc44f2b9d@ec2-50-17-194-129.compute-1.amazonaws.com:5432/dalp1drkjndrse',
-    ssl: true
+    ssl: { rejectUnauthorized: false }
   });
 
 
