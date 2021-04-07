9cd65c180c179091d1a783b9928b976e3622c553
souj <soujanya.venkatesh@gmail.com>
2021-04-07 10:57:18 +0530

    Sold Tran

--- src/app/components/car/transaction-details/transaction-details.component.ts
+++ src/app/components/car/transaction-details/transaction-details.component.ts
@@ -476,14 +476,16 @@ export class TransactionDetailsComponent implements OnInit {
           if(this.transactionDetailsForm.get('transaction_type_id').value == 13)
           {
             var sp = parseInt(this.carForm.get('selling_price').value.toString().replace( /,/g, "" ));
+            //This is the purchase price and not the CP
             var cp = parseInt(this.carForm.get('cost_price').value.toString().replace( /,/g, "" ));
             var tot = this.total_money_received + credit;
 
             console.log("souj "+cp);
+            console.log("total" +tot);
             //this credit is only calculated if transaction type is SOLD. We are adding a Credit Entry
             //as selling price - profit since we have the profit entries separately
             var credit = parseInt(this.transactionDetailsForm.get('credit').value.toString().replace( /,/g, "" ));
-
+  console.log("credit" +credit);
             if(tot>sp)
             {
               this.opMessage.emit("Sold Type transactions should not exceed the Selling Price "+this.carForm.get('selling_price').value);
@@ -498,7 +500,7 @@ export class TransactionDetailsComponent implements OnInit {
                   credit:cp,
                 });
               }
-              this.createTransactionDetails();
+            //  this.createTransactionDetails();
             }
           }
           else{
